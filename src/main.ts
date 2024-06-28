43f99b9a572b2f5a0319f42071859d25a8ddf044
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-06-27 21:38:22 -0300

    Initial discord.js configs

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
-console.log('Hello, world');
+import { Client, IntentsBitField } from 'discord.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client: Client = new Client({
+    intents: [
+        IntentsBitField.Flags.Guilds,
+        IntentsBitField.Flags.GuildMembers,
+        IntentsBitField.Flags.GuildMessages,
+        IntentsBitField.Flags.MessageContent,
+    ],
+});
+
+const token: string | undefined = process.env.TOKEN;
+
+if (!token) {
+    throw new Error('Auth token not found in environment variables!');
+}
+client.login(token);
