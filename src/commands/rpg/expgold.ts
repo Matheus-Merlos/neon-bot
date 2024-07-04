30644e897609d0f45579d161fd766351fd6c3dbc
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-07-04 14:27:16 -0300

    Optimized some bad code

--- src/commands/rpg/expgold.ts
+++ src/commands/rpg/expgold.ts
@@ -8,9 +8,9 @@ import {
 } from '../../utils';
 import db from '../../models/db';
 import { personagem } from '../../models/schema';
-import { eq } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 
-export class AddGold extends Command {
+abstract class AddResource extends Command {
     public async execute(): Promise<void> {
         const msgArray: Array<string> = this.message.content.split(' ');
         if (msgArray.length !== 3) {
@@ -34,56 +34,37 @@ export class AddGold extends Command {
         addPlayerAndCharacterIfNotExists(id, guild)
             .then(async () => {
                 await this.add(id, quantity);
-                this.message.reply('Gold adicionado com sucesso!');
+                this.message.reply('Adicionado com sucesso!');
             })
             .catch((error: Error) => {
                 this.message.reply(`Houve um herro ao incluir o jogador: ${error.message}`);
             });
     }
-    private async add(playerId: string, quantity: number) {
+    protected abstract add(playerId: string, quantity: number): Promise<void>;
+}
+
+export class AddGold extends AddResource {
+    protected async add(playerId: string, quantity: number) {
         const character: Character = await getCurrentCharacterFromId(playerId);
 
         const characterId = character.characterId;
 
-        await db.update(personagem).set({ gold: quantity }).where(eq(personagem.id, characterId));
+        await db
+            .update(personagem)
+            .set({ gold: sql`${personagem.gold}+ ${quantity}` })
+            .where(eq(personagem.id, characterId));
     }
 }
 
-export class AddExp extends Command {
-    public async execute(): Promise<void> {
-        const msgArray: Array<string> = this.message.content.split(' ');
-        if (msgArray.length !== 3) {
-            this.message.reply('A sintaxe do comando está errada');
-            return;
-        }
-
-        const mention: string = msgArray[1];
-        if (!mention.includes('@')) {
-            this.message.reply('O player informado não é um player válido');
-            return;
-        }
-
-        const id: string = getIdFromMention(mention);
-        const quantity: number = parseInt(msgArray[2]);
-        if (isNaN(quantity)) {
-            this.message.reply('A quantidade informada é inválida');
-        }
-
-        const guild: Guild = this.message.guild!;
-        addPlayerAndCharacterIfNotExists(id, guild)
-            .then(async () => {
-                await this.add(id, quantity);
-                this.message.reply('Exp adicionado com sucesso!');
-            })
-            .catch((error: Error) => {
-                this.message.reply(`Houve um herro ao incluir o jogador: ${error.message}`);
-            });
-    }
-    private async add(playerId: string, quantity: number) {
+export class AddExp extends AddResource {
+    protected async add(playerId: string, quantity: number) {
         const character: Character = await getCurrentCharacterFromId(playerId);
 
         const characterId = character.characterId;
 
-        await db.update(personagem).set({ xp: quantity }).where(eq(personagem.id, characterId));
+        await db
+            .update(personagem)
+            .set({ xp: sql`${personagem.xp}+ ${quantity}` })
+            .where(eq(personagem.id, characterId));
     }
 }
