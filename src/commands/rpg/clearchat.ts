48c39023272fced5c533ad227aa19810bd2d5f0d
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-07-09 13:56:46 -0300

    Changes to the clearchat command

--- src/commands/rpg/clearchat.ts
+++ src/commands/rpg/clearchat.ts
@@ -1,17 +1,23 @@
+import { Collection, Message } from 'discord.js';
 import Command from '../command';
 
 export default class ClearChat extends Command {
     public async execute(): Promise<void> {
-        const messages = await this.message.channel.messages.fetch({ limit: 100 });
+        const messages: Collection<
+            string,
+            Message<boolean>
+        > = await this.message.channel.messages.fetch({ limit: 100 });
 
         const deletePromises: Promise<unknown>[] = [];
         let total: number = 0;
 
-        messages.forEach((message) => {
-            if (message.content.includes('//') || message.content.includes('((')) {
-                deletePromises.push(message.delete());
-                total++;
-            }
+        const messagesToDelete: Collection<string, Message<boolean>> = messages.filter(
+            (message: Message) => message.content.includes('//') || message.content.includes('(('),
+        );
+
+        messagesToDelete.forEach((message) => {
+            deletePromises.push(message.delete());
+            total++;
         });
 
         await Promise.all(deletePromises);
