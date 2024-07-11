ff73b80f20d4f07f3e33546c952201f306948d22
Matheus-Merlos <matheusazrmerlos@gmail.com>
2024-07-11 11:46:58 -0300

    Failed attempt at doing decorators

---
+++ src/commands/decorators.ts
@@ -0,0 +1,21 @@
+import { Message, PermissionsBitField } from 'discord.js';
+import Command from './command';
+
+function hasPermission(
+    target: Command,
+    methodName: string,
+    descriptor: PropertyDescriptor,
+): PropertyDescriptor {
+    const originalMethod = descriptor.value;
+    return {
+        value: async function (...args: Array<unknown>) {
+            const message: Message = target.message;
+            if (message.member!.permissions.has(PermissionsBitField.Flags.Administrator)) {
+                return await originalMethod(args);
+            }
+            message.reply('Você não tem permissão para isso!');
+        },
+    };
+}
+
+export { hasPermission };
