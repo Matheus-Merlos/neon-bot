c8c3dec8992946614e8070e9836440c9c48656a5
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-12 16:58:05 -0300

    Added remove gold and exp commands

---
+++ src/commands/inventory/exp-gold/exp-gold.ts
@@ -0,0 +1,64 @@
+import { Message } from 'discord.js';
+import { eq } from 'drizzle-orm';
+import db from '../../../db/db';
+import { character } from '../../../db/schema';
+import CharacterFactory from '../../../factories/character-factory';
+import { getIdFromMention } from '../../../utils';
+import Command from '../../base-command';
+
+class AddRemoveCommand {
+    async execute(
+        message: Message,
+        messageAsList: Array<string>,
+        operator: '+' | '-',
+        attribute: 'gold' | 'xp',
+    ): Promise<void> {
+        if (!messageAsList[1].includes('@')) {
+            await message.reply(`O player "${messageAsList[1]}" não é válido.`);
+            return;
+        }
+
+        const playerId = getIdFromMention(messageAsList[1]);
+        const quantity = parseInt(messageAsList[2]);
+
+        const char = await CharacterFactory.getFromId(playerId, message);
+
+        if (eval(`${char[attribute]}${operator}${quantity}`) < 0) {
+            await message.reply(
+                `O personagem **${char.name}** nem possui essa quantidade de ${attribute}.`,
+            );
+            return;
+        }
+
+        type UpdateObj = {
+            [k: string]: number;
+        };
+
+        const updateObj: UpdateObj = {};
+        updateObj[attribute] = eval(`${char[attribute]}${operator}${quantity}`);
+
+        await db.update(character).set(updateObj).where(eq(character.id, char.id));
+
+        await message.reply(
+            `${quantity} de ${attribute} ${operator == '+' ? 'adicionado' : 'removido'} com sucesso ${operator == '+' ? 'para' : 'de'} **${char.name}**`,
+        );
+    }
+}
+
+export class RemoveExp extends AddRemoveCommand implements Command {
+    async execute(message: Message, messageAsList: Array<string>): Promise<void> {
+        super.execute(message, messageAsList, '-', 'xp');
+    }
+}
+
+export class AddGold extends AddRemoveCommand implements Command {
+    async execute(message: Message, messageAsList: Array<string>): Promise<void> {
+        super.execute(message, messageAsList, '+', 'gold');
+    }
+}
+
+export class RemoveGold extends AddRemoveCommand implements Command {
+    async execute(message: Message, messageAsList: Array<string>): Promise<void> {
+        super.execute(message, messageAsList, '-', 'gold');
+    }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { config } from 'dotenv';
 import Client from './client';
-import AddExp from './commands/inventory/inventory';
+import AddExp from './commands/inventory/exp-gold/add-exp';
+import { AddGold, RemoveExp, RemoveGold } from './commands/inventory/exp-gold/exp-gold';
 
 config();
 
 const addExp = new AddExp();
+const removeExp = new RemoveExp();
+const addGold = new AddGold();
+const removeGold = new RemoveGold();
 
 const client = new Client(';');
 
 client.addCommand([';addexp', ';add-exp'], addExp);
+client.addCommand([';removeexp', ';remove-exp'], removeExp);
+client.addCommand([';addgold', ';add-gold'], addGold);
+client.addCommand([';removegold', ';remove-gold'], removeGold);
