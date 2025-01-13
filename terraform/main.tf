22fc11e909f7fb27b44d4e6399774db663d2959b
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-12 21:48:05 -0300

    Added deploy action

--- terraform/main.tf
+++ terraform/main.tf
@@ -9,6 +9,20 @@ resource "aws_security_group" "ec2_security_group" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     from_port   = 0
     to_port     = 0
