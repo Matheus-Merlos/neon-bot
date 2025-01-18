4d67e699006fadd1d73f1d9db0492e7b749faa0d
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-18 17:29:21 -0300

    Test deploy job

--- src/drizzle-prod.config.ts
+++ src/drizzle-prod.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
     out: './drizzle',
-    schema: './src/db/schema.js',
+    schema: './db/schema.js',
     dialect: 'sqlite',
     dbCredentials: {
         url: process.env.DB_FILE_NAME!,
