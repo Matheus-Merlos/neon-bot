22f1cc42f5795dfa91a327990a4f70302682aec3
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-19 15:34:58 -0300

    Changed ec2 instance type and fixed deploy job

--- terraform/main_ec2.tf
+++ terraform/main_ec2.tf
@@ -33,7 +33,7 @@ resource "aws_security_group" "ec2_security_group" {
 
 resource "aws_instance" "main_server" {
     ami             = "ami-0789039e34e739d67"
-    instance_type   = "t4g.micro"
+    instance_type   = "t4g.nano"
 
     key_name        = "neon-bot-key-pair"
     security_groups = [aws_security_group.ec2_security_group.name]
@@ -46,4 +46,8 @@ resource "aws_instance" "main_server" {
 
 resource "aws_eip" "server_eip" {
   instance = aws_instance.main_server.id
+
+  tags = {
+    Name = "tf-neon-bot-server-ip"
+  }
 }
