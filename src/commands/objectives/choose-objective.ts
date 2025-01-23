73bbc3cc49e626a504d617c0d2b0b251b3925cad
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-22 21:08:05 -0300

    Fixed a error in choose-objective

--- src/commands/objectives/choose-objective.ts
+++ src/commands/objectives/choose-objective.ts
@@ -9,15 +9,15 @@ import Command from '../base-command';
 
 export default class SelectObjective implements Command {
     async execute(message: Message, messageAsList: Array<string>): Promise<void> {
-        messageAsList.splice(0, 1);
-
         let char;
-        if (messageAsList[1]) {
+        if (messageAsList[1].includes('@')) {
             char = await CharacterFactory.getFromId(getIdFromMention(messageAsList[1]), message);
             messageAsList.splice(0, 1);
         } else {
             char = await CharacterFactory.getFromId(message.author.id, message);
         }
+
+        messageAsList.splice(0, 1);
 
         const objectiveName = messageAsList.join(' ');
 
