777cd31b61724eff2e4debbac4a70d814fe92b84
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-01-24 23:13:22 -0300

    Fixed hasPermission forEach loop

--- src/decorators/has-permission.ts
+++ src/decorators/has-permission.ts
@@ -14,7 +14,7 @@ export default function hasPermission(...permissions: Array<PermissionResolvable
     return function (classPrototype: Command, methodName: string, descriptor: PropertyDescriptor) {
         const callable = descriptor.value;
         async function wrapper(message: Message, messageAsList: Array<string>) {
-            permissions.forEach(async (permission) => {
+            for (const permission of permissions) {
                 const member = await message.guild!.members.fetch(message.author.id);
                 if (member.permissions.has(permission)) {
                     await callable(message, messageAsList);
@@ -24,7 +24,7 @@ export default function hasPermission(...permissions: Array<PermissionResolvable
                         `Você não possui a permissão de **${getPermissionName(permission)}** necessária para executar este comando`,
                     );
                 }
-            });
+            }
         }
         return {
             value: wrapper,
