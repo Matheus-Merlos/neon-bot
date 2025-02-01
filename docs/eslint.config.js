c47d89af62b346289d6a5c20d2229b0359b1b2e2
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-02-01 16:51:06 -0300

    Fixed MORE eslint errors

--- docs/eslint.config.js
+++ docs/eslint.config.js
@@ -1,43 +1,21 @@
-import prettier from 'eslint-config-prettier';
 import js from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 import svelte from 'eslint-plugin-svelte';
 import globals from 'globals';
 import ts from 'typescript-eslint';
 
-import { FlatCompat } from '@eslint/eslintrc';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
 import typescriptEslint from '@typescript-eslint/eslint-plugin';
-import tsParser from '@typescript-eslint/parser';
+import svelteParser from 'svelte-eslint-parser';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [
-    ...compat.extends(
-        'eslint:recommended',
-        'plugin:@typescript-eslint/eslint-recommended',
-        'plugin:@typescript-eslint/recommended',
-        'plugin:prettier/recommended',
-        'plugin:svelte/recommended'
-    ),
+export default ts.config(
     js.configs.recommended,
-    ...ts.configs.recommended,
     ...svelte.configs['flat/recommended'],
+    ...ts.configs.recommended,
     prettier,
     ...svelte.configs['flat/prettier'],
     {
-        plugins: {
-            '@typescript-eslint': typescriptEslint,
-            svelte: svelte
-        },
-
+        files: ['**/*.svelte', '**/*.ts', '**/*.js'],
+        ignores: ['eslint.config.js'],
         languageOptions: {
             globals: {
                 ...globals.browser,
@@ -45,21 +23,22 @@ export default [
                 Atomics: 'readonly',
                 SharedArrayBuffer: 'readonly'
             },
-
-            parser: tsParser,
+            parser: svelteParser,
+            parserOptions: {
+                parser: '@typescript-eslint/parser',
+                project: './tsconfig.json',
+                ecmaVersion: 11,
+                sourceType: 'module',
+                extraFileExtensions: ['.svelte']
+            },
             ecmaVersion: 11,
             sourceType: 'module'
         },
-
-        rules: {},
-
-        files: ['*.svelte', '*.ts', '*.tsx', '*.js'],
-        processor: 'svelte/svelte',
-
-        settings: {
-            'svelte/ignore-styles': () => true // Ignorar estilos se necessário
+        plugins: {
+            '@typescript-eslint': typescriptEslint,
+            svelte
         },
-
-        ignores: ['node_modules/', '.svelte-kit/']
+        processor: 'svelte/svelte',
+        rules: {},
     }
-];
+);
--- docs/src/routes/+layout.svelte
+++ docs/src/routes/+layout.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
     import "$lib/styles/global.css";
 
+    import Header from "$lib/components/Header.svelte";
     import type { Snippet } from 'svelte';
-    import type { LayoutData } from './$types'
-	import Header from "$lib/components/Header.svelte";
+    import type { LayoutData } from './$types';
 
     let { data, children }: { data: LayoutData, children: Snippet } = $props();
 </script>
