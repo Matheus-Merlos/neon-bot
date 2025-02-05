a8e9b6c76fe84062c5d1da66ac3c0a0fae865ee7
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-02-05 20:17:57 -0300

    Added Header tests

--- docs/tests/e2e/main-page.spec.ts
+++ docs/tests/e2e/main-page.spec.ts
@@ -4,8 +4,12 @@ test.beforeEach(async ({ page }: { page: Page }) => {
     await page.goto('/');
 });
 
-test('Should redirect to commands page', async ({ page }: { page: Page }) => {
-    await page.getByRole('button', { name: 'Comandos' }).click();
+test.describe('Main page navigation testing', () => {
+    test.describe('Main buttons testing', () => {
+        test('Should redirect to commands page', async ({ page }: { page: Page }) => {
+            await page.getByRole('button', { name: 'Comandos' }).click();
 
-    await page.waitForURL(/commands/);
+            await page.waitForURL(/commands/);
+        });
+    });
 });
