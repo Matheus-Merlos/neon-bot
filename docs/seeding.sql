72d9763ad6102b8e86bfac8c2e908f1065347a96
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-02-06 20:47:31 -0300

    Fix error in test action

--- docs/seeding.sql
+++ docs/seeding.sql
@@ -4,6 +4,7 @@ INSERT INTO command VALUES(3,'Remove-Exp','remove-exp',replace('Comando para rem
 INSERT INTO command VALUES(4,'Add-Gold','add-gold',replace('Comando utilizado para adicionar ouro a um personagem específico.\n\n## Sintaxe  \n\n`;add-gold <@menção> <quantidade>`\n\n- **@Menção**: A menção do jogador cujo personagem receberá o ouro. Caso o personagem ainda não exista, ele será automaticamente criado, com a foto de perfil e o apelido do servidor.\n- **Quantidade**: A quantidade (em número) de ouro a ser adicionada ao personagem.\n','\n',char(10)),1);
 INSERT INTO command VALUES(5,'Remove-Gold','remove-gold',replace('Comando usado para remover ouro de um personagem específico.\n\n## Sintaxe  \n\n`;remove-gold <@menção> <quantidade>`\n\n- **@Menção**: A menção do jogador cujo personagem perderá o ouro. Se o personagem não existir, o comando não terá efeito.\n- **Quantidade**: A quantidade (em número) de ouro a ser removida do personagem.\n','\n',char(10)),1);
 
+
 INSERT INTO family VALUES(1,'Inventário');
 INSERT INTO family VALUES(2,'Objetivos');
 INSERT INTO family VALUES(3,'Classes');
@@ -11,6 +12,7 @@ INSERT INTO family VALUES(4,'Objetivos de Classes');
 INSERT INTO family VALUES(5,'Itens');
 INSERT INTO family VALUES(6,'Missões');
 INSERT INTO family VALUES(7,'Uso Geral');
+
 
 INSERT INTO sqlite_sequence VALUES('family',7);
 INSERT INTO sqlite_sequence VALUES('command',5);
