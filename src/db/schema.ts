956f69dbbbfa840bbb93b1c793528ee655a087e3
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-03-29 16:16:55 -0300

    feat(db): Changed entire schema to postgresql

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,47 +1,47 @@
-import { blob, int, sqliteTable, text } from 'drizzle-orm/sqlite-core';
+import { bigint, boolean, integer, pgTable, serial, text, varchar } from 'drizzle-orm/pg-core';
 
-export const player = sqliteTable('player', {
-    discordId: blob('discord_id', { mode: 'bigint' }).primaryKey(),
+export const player = pgTable('player', {
+    discordId: bigint('discord_id', { mode: 'bigint' }).primaryKey(),
 });
 
-export const rank = sqliteTable('rank', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    necessaryXp: int('xp').notNull(),
-    extraAttributes: int('extra_attributes').notNull(),
-    extraHabs: int('extra_habs').notNull(),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+export const rank = pgTable('rank', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 31 }).notNull(),
+    necessaryXp: integer('xp').notNull(),
+    extraAttributes: integer('extra_attributes').notNull(),
+    extraHabs: integer('extra_habs').notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const reachedRank = sqliteTable('reached_rank', {
-    id: int('id').primaryKey({ autoIncrement: true }),
-    characterId: int('character_id')
+export const reachedRank = pgTable('reached_rank', {
+    id: serial('id'),
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    rankId: int('rank_id')
+    rankId: integer('rank_id')
         .notNull()
         .references(() => rank.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
 });
 
-export const item = sqliteTable('item', {
-    id: int('id').primaryKey({ autoIncrement: true }),
-    name: text('name').notNull(),
+export const item = pgTable('item', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 127 }).notNull(),
     description: text('description'),
-    price: int('price').notNull(),
-    durability: int('durability').notNull(),
-    canBuy: int('can_buy', { mode: 'boolean' }).notNull(),
-    image: text('image'),
-    salt: text('salt', { length: 5 }),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+    price: integer('price').notNull(),
+    durability: integer('durability').notNull(),
+    canBuy: boolean('can_buy').notNull(),
+    image: varchar('image', { length: 255 }),
+    salt: varchar('salt', { length: 5 }),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const inventory = sqliteTable('inventory', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    durability: int('durability').notNull(),
-    characterId: int('character_id')
+export const inventory = pgTable('inventory', {
+    id: serial('id').primaryKey(),
+    durability: integer('durability').notNull(),
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade' }),
-    itemId: int('item_id')
+    itemId: integer('item_id')
         .notNull()
         .references(() => item.id, { onDelete: 'cascade' }),
 });
@@ -49,119 +49,119 @@ export const inventory = sqliteTable('inventory', {
 /*
 Objective System
 */
-export const objectiveDifficulty = sqliteTable('objective_difficulty', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+export const objectiveDifficulty = pgTable('objective_difficulty', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 31 }).notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const objective = sqliteTable('objective', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    xp: int('xp').notNull(),
-    gold: int('gold').notNull(),
+export const objective = pgTable('objective', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 127 }).notNull(),
+    xp: integer('xp').notNull(),
+    gold: integer('gold').notNull(),
     description: text('description').notNull(),
-    type: int('type')
+    type: integer('type')
         .notNull()
         .references(() => objectiveDifficulty.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const selectedObjective = sqliteTable('selected_objective', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    objectiveId: int('objective_id')
+export const selectedObjective = pgTable('selected_objective', {
+    id: serial('id').primaryKey(),
+    objectiveId: integer('objective_id')
         .notNull()
         .references(() => objective.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    characterId: int('character_id')
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
 });
 
-export const completedObjective = sqliteTable('completed_objective', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    objectiveId: int('objective_id')
+export const completedObjective = pgTable('completed_objective', {
+    id: serial('id').primaryKey(),
+    objectiveId: integer('objective_id')
         .notNull()
         .references(() => objective.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    characterId: int('character_id')
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
 });
 
-export const characterClass = sqliteTable('class', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+export const characterClass = pgTable('class', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 127 }).notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const classObjective = sqliteTable('class_objective', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    xp: int('xp').notNull(),
-    gold: int('gold').notNull(),
+export const classObjective = pgTable('class_objective', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 127 }).notNull(),
+    xp: integer('xp').notNull(),
+    gold: integer('gold').notNull(),
     description: text('description').notNull(),
-    classId: int('class')
+    classId: integer('class')
         .notNull()
         .references(() => characterClass.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const character = sqliteTable('character', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    xp: int('xp').notNull(),
-    gold: int('gold').notNull(),
-    player: blob('player', { mode: 'bigint' })
+export const character = pgTable('character', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
+    xp: integer('xp').notNull(),
+    gold: integer('gold').notNull(),
+    player: bigint('player', { mode: 'bigint' })
         .references(() => player.discordId, {
             onDelete: 'cascade',
         })
         .notNull(),
-    imageUrl: text('image_url'),
-    salt: text('salt', { length: 5 }),
-    characterClass: int('class').references(() => characterClass.id, { onDelete: 'set null', onUpdate: 'no action' }),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+    imageUrl: varchar('image_url', { length: 255 }),
+    salt: varchar('salt', { length: 5 }),
+    characterClass: integer('class').references(() => characterClass.id, { onDelete: 'set null', onUpdate: 'no action' }),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const completedClassObjective = sqliteTable('completed_class_objective', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    characterId: int('character_id')
+export const completedClassObjective = pgTable('completed_class_objective', {
+    id: serial('id').primaryKey(),
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    classObjectiveId: int('class_objective_id')
+    classObjectiveId: integer('class_objective_id')
         .notNull()
         .references(() => classObjective.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    completed: int('completed', { mode: 'boolean' }).default(false).notNull(),
+    completed: boolean('completed').default(false).notNull(),
 });
 
 /*
 Mission System
 */
-export const missionDifficulty = sqliteTable('mission_difficulty', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
+export const missionDifficulty = pgTable('mission_difficulty', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 31 }).notNull(),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
 });
 
-export const mission = sqliteTable('mission', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    name: text('name').notNull(),
+export const mission = pgTable('mission', {
+    id: serial('id').primaryKey(),
+    name: varchar('name', { length: 255 }).notNull(),
     description: text('description').notNull(),
-    xp: int('xp').notNull(),
-    gold: int('gold').notNull(),
-    difficulty: int('difficulty')
+    xp: integer('xp').notNull(),
+    gold: integer('gold').notNull(),
+    difficulty: integer('difficulty')
         .notNull()
         .references(() => missionDifficulty.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    imageUrl: text('image_url'),
-    salt: text('salt', { length: 5 }),
-    guildId: blob('guild_id', { mode: 'bigint' }).notNull(),
-    completed: int('completed', { mode: 'boolean' }).notNull().default(false),
+    imageUrl: varchar('image_url', { length: 255 }),
+    salt: varchar('salt', { length: 5 }),
+    guildId: bigint('guild_id', { mode: 'bigint' }).notNull(),
+    completed: boolean('completed').notNull().default(false),
 });
 
-export const missionComplete = sqliteTable('mission_complete', {
-    id: int('id').primaryKey({ autoIncrement: true }).notNull(),
-    characterId: int('character_id')
+export const missionComplete = pgTable('mission_complete', {
+    id: serial('id').primaryKey(),
+    characterId: integer('character_id')
         .notNull()
         .references(() => character.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
-    missionId: int('mission_id')
+    missionId: integer('mission_id')
         .notNull()
         .references(() => mission.id, { onDelete: 'cascade', onUpdate: 'cascade' }),
 });
