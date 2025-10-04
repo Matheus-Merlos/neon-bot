ca1a411a91e848f48839607339b45b0e25706377
Matheus-Merlos <matheusazrmerlos@gmail.com>
2025-10-04 19:54:12 -0300

    feat(docs): Removed docs configuration and gitattributes

--- docs/src/lib/db/index.ts
+++
@@ -1,7 +0,0 @@
-import Database from 'better-sqlite3';
-import { config } from 'dotenv';
-import { drizzle } from 'drizzle-orm/better-sqlite3';
-config();
-if (!process.env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = new Database(process.env.DATABASE_URL);
-export const db = drizzle(client);
